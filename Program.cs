cd1927800e7b7f56b519540cacedabcee880466b
d052057 <d052057@yahoo.com>
2022-09-11 11:28:45 -0700

    change a.flac

--- Program.cs
+++ Program.cs
@@ -5,7 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddCors(
+             options =>
+             {
+                 options.AddPolicy("CorsPolicy", builder => builder
+                    .WithOrigins("*")
+                    .AllowAnyOrigin()
+                    .SetIsOriginAllowed(origin => true)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    //.AllowCredentials()
+                    //.AllowCredentials() and AllowAnyOrigin() can not be used at the same time
+                    );
+             }
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,14 +26,21 @@ if (!app.Environment.IsDevelopment())
 {
 }
 
-app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions()
+{
+    //FileProvider = new PhysicalFileProvider(@"c:/medias"),
+    //RequestPath = new PathString("/assets/medias")  
+});
 app.UseFileServer(new FileServerOptions()
 {
     // configuration["ApplicationSettings:MEDIAS"].ToString() is c:/medias or d:/medias
     FileProvider = new PhysicalFileProvider(@"c:/medias"),
     RequestPath = new Microsoft.AspNetCore.Http.PathString(@"/assets/medias"),
-    EnableDirectoryBrowsing = false // you make this true or false.
+    EnableDirectoryBrowsing = true // you make this true or false.
 });
+
+app.UseCors("CorsPolicy");
 app.UseRouting();
 
 
